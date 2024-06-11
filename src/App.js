eec65e629e44aed954addf29d12c42dedcbc9dbd
shy8818 <83875932+shyhwan@users.noreply.github.com>
2024-06-12 06:38:04 +0900

    movie app part one

--- src/App.js
+++ src/App.js
@@ -1,6 +1,52 @@
 import { useState, useEffect } from 'react';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`);
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getMovies();
+        /* 요즘은 이 방식 말고 async-await를 사용 하기도 한다.
+        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+            .then((response) => response.json())
+            .then((json) => {
+                setMovies(json.data.movies);
+                setLoading(false);
+            });
+        */
+    }, []);
+    console.log(movies);
+    return (
+        <div>
+            {loading ? (
+                <h1>Loding...</h1>
+            ) : (
+                <div>
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            <img src={movie.medium_cover_image} />
+                            <h2>{movie.title}</h2>
+                            <p>{movie.summary}</p>
+                            <ul>
+                                {movie.genres.map((g) => (
+                                    <li key={g}>{g}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+
+/* Todo
+function App() {
     const [toDo, setToDo] = useState('');
     const [toDos, setToDos] = useState([]);
     const onChange = (e) => setToDo(e.target.value);
@@ -28,7 +74,7 @@ function App() {
         </div>
     );
 }
-
+*/
 /*
 function Hello() {
     // useEffect(); content가 바뀌어도 한번만 실행
